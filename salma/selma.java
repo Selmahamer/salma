448e24404047a5d7fc2abe6cda5d336c230fbaf7
salma hamer <salma hamer@LAPTOP-S5CHACUO>
2023-12-12 16:31:51 +0100

    cfffff

--- salma/selma.java
+++ salma/selma.java
@@ -3,7 +3,8 @@ public class selma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("salut çava");
+System.out.println("salut çava");
 	}
 
 }
